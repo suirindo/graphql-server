0f55b30d3f7008943fe88bf8eb12ea31975c17da
asagiman <lagavulin.jb.dc@gmail.com>
2022-02-08 21:54:25 +0900

    環境構築

--- schema.js
+++ schema.js
@@ -1,7 +1,7 @@
 const{gql} = require('apollo-server');
 
 const typeDefs = gql`
-typw Query {
+type Query {
     # nullの可能性がないリストで値はnullではない文字列型
     posts(query: String): [Post!]!
 }
@@ -13,7 +13,36 @@ type Mutation {
 }
 
 # サブスクリプション
+type Subscription {
+    post: PostSubscriptionPayload!
+}
 
+input CreatePostInput {
+    title: String!
+    author: String!
+}
 
+input UpdatePostInput {
+    title: String
+    author: String!
+}
 
+type Post {
+    id: ID!
+    title: String!
+    author: String!
+}
+
+enum MutationType {
+    CREATED
+    UPDATED
+    DELETED
+}
+
+#サブスクリプションのフィールド
+type PostSubscriptionPayload {
+    mutation: MutationType!
+    data: Post!
+}
 `
+module.exports = typeDefs;
