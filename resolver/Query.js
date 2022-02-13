c123d2b77b1310cd06223d1a314dbc739f01e3a3
asagiman <lagavulin.jb.dc@gmail.com>
2022-02-13 23:17:09 +0900

    1

--- resolver/Query.js
+++ resolver/Query.js
@@ -1,6 +1,38 @@
 // リゾルバ関数の用意
 // 引数の有無で条件分岐をしている
 
+import gql from 'graphql-tag'
+
+export const ALL_POSTS = gql`
+query{
+    posts{
+        id
+        title
+        author
+        postedUser{
+            id
+            name
+        }
+        updatedAt
+        createdAt
+    }
+}
+`
+
+export const ALL_USERS = gql`
+query{
+    users{
+        id
+        name
+        email
+        updatedAt
+    posts{
+        title
+        author
+    }
+    }
+}
+`
 const { printSchema } = require("graphql")
 
 const Query = {
